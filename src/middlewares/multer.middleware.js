f95b23707257c84da94e51466cf0da72e56672a0
Mayank06711 <mayank93soni@gmail.com>
2024-03-13 03:29:15 +0530

    cloudinary in utils  and multer in middleware is added

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files
+// ----------------------------------------------------------------
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // cb means callback on docs
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+});
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,16 +3,16 @@ class ApiError extends Error {
       statusCode, // HTTP status code for the error
       message = "Something went wrong apierror", // Error message (default value provided)
       errors = [], // Array of error details or validation errors (default value provided)
-      statck = "" // Stack trace for the error (default value provided)
+      stack = "" // Stack trace for the error (default value provided)
     ) {
       super(message); // Call the constructor of the Error class with the provided message
       this.statusCode = statusCode; // Assign the provided statusCode to the statusCode property of the instance
       this.data = null; // Initialize data property to null (can be used to attach additional data)
       this.errors = errors; // Assign the provided errors to the errors property of the instance
-      this.statck = statck; // Assign the provided stack trace to the statck property of the instance
+      this.stack = stack; // Assign the provided stack trace to the statck property of the instance
       this.success = false; // Indicate that the operation resulting in the error was not successful
-      if (statck) {
-        this.statck = statck; // If a stack trace is provided, assign it to the statck property
+      if (stack) {
+        this.stack = stack; // If a stack trace is provided, assign it to the statck property
       } else {
         Error.captureStackTrace(this, this.constructor); // Otherwise, capture the stack trace
       }
