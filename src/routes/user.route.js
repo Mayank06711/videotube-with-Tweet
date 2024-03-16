78ec12f9675dfc04b0adbd2f7a077b6302b127fa
Mayank06711 <mayank93soni@gmail.com>
2024-03-17 03:15:22 +0530

    new routes for userControll

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessTooken } from "../controllers/user.controller.js";
+import {
+        registerUser,
+        loginUser, 
+        logoutUser, 
+        refreshAccessTooken, 
+        changeCurrentPassword, 
+        getCurrentUser, 
+        updateUserDetails, 
+        updateUserAvatar, 
+        updateUserCoverImage, 
+        getUserChannelProfile, 
+        getWatchHistory 
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -28,7 +40,31 @@ router.route("/login").post(loginUser);
 // secure routes
 // ----------- logout user
 router.route("/logout").post(verifyJWT ,logoutUser);
+
 router.route("/refresh-token").post(refreshAccessTooken);
 
+router.route("/change-password").port(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router
+.route("/update-account")
+.patch(verifyJWT, updateUserDetails) //if post all details will be chnaged
+
+router
+.route("/avatar")
+.patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+
+router
+.route("/cover-image")
+.patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router
+.route("/c/:username")
+.get(verifyJWT, getUserChannelProfile) // here we added route as username same as we have in channelprofile bcz we are getting this in userChannel thoru req.params i.e url
+
+router
+.route("/history")
+.get(verifyJWT,getWatchHistory)
 
 export default  router ;
