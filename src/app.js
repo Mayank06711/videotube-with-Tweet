2ad0ee5f6a81e525af2a4fc3b82b4b5694b54b16
Mayank06711 <mayank93soni@gmail.com>
2024-03-17 13:31:17 +0530

    added prototypes of all routes and controllers

--- src/app.js
+++ src/app.js
@@ -33,12 +33,28 @@ app.use(cookieParser()); // to perfomr CRUD OPER ON USER WEB COOKIES
 
 // importing routes
 import userRouter from "./routes/user.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import videoRouter from "./routes/video.route.js"
+import playlistRouter from "./routes/playlist.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
+import healthcheckRouter from "./routes/healthcheck.route.js"
+import dashboardRouter from "./routes/dashboard.route.js"
+
 
 // route declarations
 
 // Mounting the userRouter middleware at the "/api/v1/users" endpoint
 app.use("/api/v1/users", userRouter);   // here api/v1/users routes will act as prefix and controll will go to userRegister
-
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost/api/v1/users/*
 export { app };
