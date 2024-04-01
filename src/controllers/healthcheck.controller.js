ec9847a0bd56ec684ac91b856c250887952a5b13
Mayank06711 <mayank93soni@gmail.com>
2024-04-01 11:38:04 +0530

    Health of healthCheck controller has been fixed by removing its bugs

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
@@ -6,13 +7,16 @@ import asyncHandler from "../utils/asyncHandler.js"
 const healthCheck = asyncHandler(async (req, res) => {
     try {
         // Check database connectivity
+
         await mongoose.connection.db.admin().ping();
 
         // If the ping succeeds, respond with a 200 status
+
         res.json(new ApiResponse(200, { status: "OK", message: "Service is running and operational" }));
+
     } catch (error) {
         // If an error occurs, indicate that the service is not operational
-        throw new ApiError(500, "Database connection failed on healthCheck");
+        throw new ApiError(500, error, "Database connection failed on healthCheck");
     }
 });//DONE!
 
