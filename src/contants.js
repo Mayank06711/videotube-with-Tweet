090be89d416d6926b95872f8b85e0ce95fb10bc0
Mayank06711 <mayank93soni@gmail.com>
2024-04-07 23:59:12 +0530

    added comment to add feature in future versions of the videotube

--- src/contants.js
+++ src/contants.js
@@ -1,2 +1,11 @@
 export const DB_NAME = "videotube"
 export const DB_PASSWORD = "LkjtCCSuWV8qPS1r"  // password for authentication (default of user name mayank93soni ans bkcendyoutueb
+/* 
+
+video ko direct cloudnary se nii lena hai 
+
+//video ko stream krna hai 
+
+//express micro service krna hai 
+
+*/
