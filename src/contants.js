a0d98277fb7b68ec8ba32f903db22cc18f23b611
Mayank06711 <mayank93soni@gmail.com>
2024-04-16 17:00:57 +0530

    merged

--- src/contants.js
+++ src/contants.js
@@ -1,9 +1,5 @@
 export const DB_NAME = "videotube"
-<<<<<<< HEAD
-
-=======
 export const DB_PASSWORD = "this is removed and can be seen in .env"  // password for authentication (default of user name mayank93soni ans bkcendyoutueb
->>>>>>> a9b0761ea6ae7ff7e6692b0b8272d6df286ba1c0
 /* 
 
 video ko direct cloudnary se nii lena hai 
